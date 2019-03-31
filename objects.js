1e5e88348ae4ae048d74e2344cc1c9e25b2f8dfe
Abagail Hudson <abagailanne@gmail.com>
2019-03-31 01:48:57 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {Madonna: 'Material Girl'} //the key will need to be one word, implying the artist name will need to be one word
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, {artistName: [songTitle]})
+  return Object.assign({}, playlist, {artistName: songTitle})
 }
