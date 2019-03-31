4d07e57973198bed8f285b5e6aaf6f108233e4a2
Abagail Hudson <abagailanne@gmail.com>
2019-03-31 01:40:08 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,5 @@
-var playlist = {Madonna: 'Material Girl'} //limitations will include the key will need to be one word, implying the artist name will need to be one word
+var playlist = {Madonna: 'Material Girl'} //the key will need to be one word, implying the artist name will need to be one word
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist,artistName,songTitle)
+}
